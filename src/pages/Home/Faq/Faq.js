b691914f66edf2506db826714572463565af213f
coder71 <kamrunnaher877@gmail.com>
2021-10-30 18:40:30 +0600

    faq section added

--- src/pages/Home/Faq/Faq.js
+++ src/pages/Home/Faq/Faq.js
@@ -1,7 +1,61 @@
-import React from 'react';
+import { Accordion, Col, Row } from 'react-bootstrap';
+import faq from '../../../images/faq.png';
 
 const Faq = () => {
-  return <div>this is some faq</div>;
+  return (
+    <div>
+      <h3 className="text-center">Frequently Asked Questions</h3>
+
+      <Row>
+        {/* faqs */}
+        <Col>
+          <Accordion defaultActiveKey="0">
+            {/* faq 1 */}
+            <Accordion.Item eventKey="0">
+              <Accordion.Header>What if we reject your order.</Accordion.Header>
+              <Accordion.Body>
+                Don't worry about it. If you give wrong information about your
+                address or credit card, we can reject your order. But if it's
+                our mistake, we are sorry for that inconvenience. You can
+                contact with our customer care service. They will help you to
+                get your money back or you can order again.
+              </Accordion.Body>
+            </Accordion.Item>
+
+            {/* faq 2 */}
+            <Accordion.Item eventKey="1">
+              <Accordion.Header>
+                What if I didn't recieve my delivered food.
+              </Accordion.Header>
+              <Accordion.Body>
+                We are extremely sorry for that. But nothing to worry about it.
+                Just inform us That you didn't recieve the ordered food. We will
+                take necessary action.
+              </Accordion.Body>
+            </Accordion.Item>
+
+            {/* faq 3 */}
+            <Accordion.Item eventKey="2">
+              <Accordion.Header>
+                Can I get the food ordered in less than an hour
+              </Accordion.Header>
+              <Accordion.Body>
+                Yes you can. It is possible beacuse our branches are located in
+                many areas. When you order something from us. We inform our
+                local branches that are situated near you. They will deliver
+                your ordered food.
+              </Accordion.Body>
+            </Accordion.Item>
+          </Accordion>
+        </Col>
+
+        {/* faq image */}
+        <Col>
+          <img src={faq} alt="faq" />
+        </Col>
+      </Row>
+    </div>
+  );
 };
 
 export default Faq;
